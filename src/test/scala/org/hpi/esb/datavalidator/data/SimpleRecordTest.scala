dde37f618c47b175c1c4c85394ddd622d18c5e82
Benjamin Reissaus <benjamin.reissaus@gmail.com>
2017-03-01 09:58:09 +0100

    add test case for SimpleRecord

--- src/test/scala/org/hpi/esb/datavalidator/data/SimpleRecordTest.scala
+++ src/test/scala/org/hpi/esb/datavalidator/data/SimpleRecordTest.scala
@@ -3,8 +3,14 @@ package org.hpi.esb.datavalidator.data
 import org.scalatest.FunSuite
 
 class SimpleRecordTest extends FunSuite {
-  test("testCreate") {
+  test("testCreate - Successful") {
     val r = SimpleRecord.deserialize("1000").get
     assert(r.value == 1000)
+  }
+
+  test("testCreate - Unsuccessful") {
+    assertThrows[IllegalArgumentException] {
+      SimpleRecord.deserialize("incorrect string").get
+    }
   }
 }
