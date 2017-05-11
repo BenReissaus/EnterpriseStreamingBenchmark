f2708e80038292bbdcfa8682666030f8608c2ee8
BenReissaus <benjamin.reissaus@gmail.com>
2017-05-11 13:00:52 +0200

    add ID to kafka consumer groupId (#85)

--- implementation/flink/cluster/src/main/scala/org/hpi/esb/flink/kafka/KafkaConsumer.scala
+++ implementation/flink/cluster/src/main/scala/org/hpi/esb/flink/kafka/KafkaConsumer.scala
@@ -1,5 +1,7 @@
 package org.hpi.esb.flink.kafka
 
+import java.util.concurrent.atomic.AtomicInteger
+
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer010
 import org.apache.flink.streaming.util.serialization.SimpleStringSchema
@@ -7,11 +9,15 @@ import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.flink.api.scala._
 import org.hpi.esb.flink.CustomAssigner
 
+object KafkaConsumer {
+  var id = new AtomicInteger(0)
+}
 class KafkaConsumer(env: StreamExecutionEnvironment, consumerTopic: String) extends KafkaConnector {
 
+  val id = KafkaConsumer.id.incrementAndGet()
   // TODO: read properties from file
   props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS)
-  props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, s"Flink_ESB - ${System.currentTimeMillis}")
+  props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, s"Flink_ESB - $id - ${System.currentTimeMillis}")
   props.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
 
   val stream = env
